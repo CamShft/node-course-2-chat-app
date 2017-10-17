475c622e59aaa38d025cf4ba5df6ce9ced474b3b
Cote <samuel.cote@cgi.com>
2017-10-17 11:32:46 -0400

    Update createMessage!

--- public/js/index.js
+++ public/js/index.js
@@ -2,15 +2,7 @@ var socket = io();
 
 socket.on('connect',function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage',{
-    from:'samy@testing.com',
-    text:'Testing.. 1..2....3 Testing'
-  });
 });
-
-
-
 socket.on('disconnect',function () {
   console.log('disconnect from server');
 });
